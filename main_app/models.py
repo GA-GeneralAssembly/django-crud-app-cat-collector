b6f09c52473bef35be40ebc679b0f01b7afeacd0
HussainWorld <164430022+HussainWorld@users.noreply.github.com>
2025-04-20 20:10:53 +0300

    adding feedigs - adding delete edit add cat to the db - adding details page fot the cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+# A tuple of 2-tuples added above our models
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 
 class Cat(models.Model):
   name = models.CharField(max_length=100)
@@ -10,3 +20,28 @@ class Cat(models.Model):
 
   def __str__(self):
     return self.name
+  
+
+  # Define a method to get the URL for this particular cat instance
+  def get_absolute_url(self):
+    return reverse('cat-detail', kwargs={'cat_id': self.id}) 
+    # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+
+
+class Feeding(models.Model):
+    # The first optional positional argument overrides the label
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+
+    # Create a cat_id column for each feeding in the database
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
@@ -23,3 +24,20 @@ def cat_index(request):
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    # success_url = '/cats/'
+    # fields = ['name', 'breed', 'description', 'age']
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+    
